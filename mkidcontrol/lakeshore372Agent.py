fc0e62a21a05496af6618ddf4db6158f2e2651d2
Noah Swimmer <nswimmer@physics.ucsb.edu>
2022-05-06 11:47:42 -0700

    LakeShore 372 commands dict creation and command structure first pass

--- mkidcontrol/lakeshore372Agent.py
+++ mkidcontrol/lakeshore372Agent.py
@@ -59,10 +59,193 @@ VALID_MODELS = ('MODEL372', )
 log = logging.getLogger()
 log.setLevel("DEBUG")
 
-ENABLED_INPUT_CHANNELS = ["A", "1"]
-ENABLED_OUTPUT_CHANNELS = [0]
+ENABLED_INPUT_CHANNELS = ("A")
+ALLOWED_INPUT_CHANNELS = ("A", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16")
+ENABLED_OUTPUT_CHANNELS = (0, )
+ALLOWED_OUTPUT_CHANNELS = (0, 1, 2)
 
-COMMANDS372 = {'device-settings:ls372:input-channel-a:': {'command': '', 'vals': ''}}
+COMMANDS372 = {}
+COMMANDS372.update({f'device-settings:ls372:input-channel-{ch.lower()}:mode': {'command': 'INTYPE', 'vals': {'VOLTAGE': 0, 'CURRENT': 1}} for ch in ALLOWED_INPUT_CHANNELS})
+COMMANDS372.update({f'device-settings:ls372:input-channel-{ch.lower()}:excitation-range': {'command': 'INTYPE', 'vals': {'RANGE_2_MICRO_VOLTS': 1,
+ 'RANGE_6_POINT_32_MICRO_VOLTS': 2,
+ 'RANGE_20_MICRO_VOLTS': 3,
+ 'RANGE_63_POINT_2_MICRO_VOLTS': 4,
+ 'RANGE_200_MICRO_VOLTS': 5,
+ 'RANGE_632_MICRO_VOLTS': 6,
+ 'RANGE_2_MILLI_VOLTS': 7,
+ 'RANGE_6_POINT_32_MILLI_VOLTS': 8,
+ 'RANGE_20_MILLI_VOLTS': 9,
+ 'RANGE_63_POINT_2_MILLI_VOLTS': 10,
+ 'RANGE_200_MILLI_VOLTS': 11,
+ 'RANGE_632_MILLI_VOLTS': 12,
+ 'RANGE_1_PICO_AMP': 1,
+ 'RANGE_3_POINT_16_PICO_AMPS': 2,
+ 'RANGE_10_PICO_AMPS': 3,
+ 'RANGE_31_POINT_6_PICO_AMPS': 4,
+ 'RANGE_100_PICO_AMPS': 5,
+ 'RANGE_316_PICO_AMPS': 6,
+ 'RANGE_1_NANO_AMP': 7,
+ 'RANGE_3_POINT_16_NANO_AMPS': 8,
+ 'RANGE_10_NANO_AMPS': 9,
+ 'RANGE_31_POINT_6_NANO_AMPS': 10,
+ 'RANGE_100_NANO_AMPS': 11,
+ 'RANGE_316_NANO_AMPS': 12,
+ 'RANGE_1_MICRO_AMP': 13,
+ 'RANGE_3_POINT_16_MICRO_AMPS': 14,
+ 'RANGE_10_MICRO_AMPS': 15,
+ 'RANGE_31_POINT_6_MICRO_AMPS': 16,
+ 'RANGE_100_MICRO_AMPS': 17,
+ 'RANGE_316_MICRO_AMPS': 18,
+ 'RANGE_1_MILLI_AMP': 19,
+ 'RANGE_3_POINT_16_MILLI_AMPS': 20,
+ 'RANGE_10_MILLI_AMPS': 21,
+ 'RANGE_31_POINT_6_MILLI_AMPS': 22}} for ch in ALLOWED_INPUT_CHANNELS})
+COMMANDS372.update({f'device-settings:ls372:input-channel-{ch.lower()}:auto-range': {'command': 'INTYPE', 'vals': {'OFF': 0, 'CURRENT': 1, 'ROX102B': 2}} for ch in ALLOWED_INPUT_CHANNELS})
+COMMANDS372.update({f'device-settings:ls372:input-channel-{ch.lower()}:current-source-shunted': {'command': 'INTYPE', 'vals': {'EXCITATION OFF': False, 'EXCITATION ON': True}} for ch in ALLOWED_INPUT_CHANNELS})
+COMMANDS372.update({f'device-settings:ls372:input-channel-{ch.lower()}:units': {'command': 'INTYPE', 'vals': {'KELVIN': 1, 'OHMS': 2}} for ch in ALLOWED_INPUT_CHANNELS})
+COMMANDS372.update({f'device-settings:ls372:input-channel-{ch.lower()}:resistance-range': {'command': 'INTYPE', 'vals': {'RANGE_2_MILLI_OHMS': 1,
+ 'RANGE_6_POINT_32_MILLI_OHMS': 2,
+ 'RANGE_20_MILLI_OHMS': 3,
+ 'RANGE_63_POINT_2_MILLI_OHMS': 4,
+ 'RANGE_200_MILLI_OHMS': 5,
+ 'RANGE_632_MILLI_OHMS': 6,
+ 'RANGE_2_OHMS': 7,
+ 'RANGE_6_POINT_32_OHMS': 8,
+ 'RANGE_20_OHMS': 9,
+ 'RANGE_63_POINT_2_OHMS': 10,
+ 'RANGE_200_OHMS': 11,
+ 'RANGE_632_OHMS': 12,
+ 'RANGE_2_KIL_OHMS': 13,
+ 'RANGE_6_POINT_32_KIL_OHMS': 14,
+ 'RANGE_20_KIL_OHMS': 15,
+ 'RANGE_63_POINT_2_KIL_OHMS': 16,
+ 'RANGE_200_KIL_OHMS': 17,
+ 'RANGE_632_KIL_OHMS': 18,
+ 'RANGE_2_MEGA_OHMS': 19,
+ 'RANGE_6_POINT_32_MEGA_OHMS': 20,
+ 'RANGE_20_MEGA_OHMS': 21,
+ 'RANGE_63_POINT_2_MEGA_OHMS': 22}} for ch in ALLOWED_INPUT_CHANNELS})
+COMMANDS372.update({f'device-settings:ls372:input-channel-{ch.lower()}:enable': {'command': 'INSET', 'vals': {'ENABLED': True, 'DISABLED': False}} for ch in ALLOWED_INPUT_CHANNELS})
+COMMANDS372.update({f'device-settings:ls372:input-channel-{ch.lower()}:dwell-time': {'command': 'INSET', 'vals': [0, 200]} for ch in ALLOWED_INPUT_CHANNELS})
+COMMANDS372.update({f'device-settings:ls372:input-channel-{ch.lower()}:pause-time': {'command': 'INSET', 'vals': [3, 200]} for ch in ALLOWED_INPUT_CHANNELS})
+COMMANDS372.update({f'device-settings:ls372:input-channel-{ch.lower()}:curve-number': {'command': 'INSET', 'vals': {str(cn): cn for cn in np.arange(1,60)}} for ch in ALLOWED_INPUT_CHANNELS})
+COMMANDS372.update({f'device-settings:ls372:input-channel-{ch.lower()}:temperatur-coefficient': {'command': 'INSET', 'vals': {'NEGATIVE': 1, 'POSITIVE': 2}} for ch in ALLOWED_INPUT_CHANNELS})
+COMMANDS372.update({f'device-settings:ls372:heater-channel-{ch}:output-mode': {'command': 'OUTMODE', 'vals': {'OFF': 0,
+ 'MONITOR_OUT': 1,
+ 'OPEN_LOOP': 2,
+ 'ZONE': 3,
+ 'STILL': 4,
+ 'CLOSED_LOOP': 5,
+ 'WARMUP': 6}} for ch in ALLOWED_OUTPUT_CHANNELS})
+COMMANDS372.update({f'device-settings:ls372:heater-channel-{ch}:input-channel': {'command': 'OUTMODE', 'vals': {'NONE': 0,
+ 'ONE': 1,
+ 'TWO': 2,
+ 'THREE': 3,
+ 'FOUR': 4,
+ 'FIVE': 5,
+ 'SIX': 6,
+ 'SEVEN': 7,
+ 'EIGHT': 8,
+ 'NINE': 9,
+ 'TEN': 10,
+ 'ELEVEN': 11,
+ 'TWELVE': 12,
+ 'THIRTEEN': 13,
+ 'FOURTEEN': 14,
+ 'FIFTEEN': 15,
+ 'SIXTEEN': 16,
+ 'CONTROL': 'A'}} for ch in ALLOWED_OUTPUT_CHANNELS})
+COMMANDS372.update({f'device-settings:ls372:heater-channel-{ch}:powerup-enable': {'command': 'OUTMODE', 'vals': {'ENABLED': True, 'DISABLED': False}} for ch in ALLOWED_OUTPUT_CHANNELS})
+COMMANDS372.update({f'device-settings:ls372:heater-channel-{ch}:reading-filter': {'command': 'OUTMODE', 'vals': {'ENABLED': True, 'DISABLED': False}} for ch in ALLOWED_OUTPUT_CHANNELS})
+COMMANDS372.update({f'device-settings:ls372:heater-channel-{ch}:delay': {'command': 'OUTMODE', 'vals': [1, 255]} for ch in ALLOWED_OUTPUT_CHANNELS})
+COMMANDS372.update({f'device-settings:ls372:heater-channel-{ch}:polarity': {'command': 'OUTMODE', 'vals': {'UNIPOLAR': 0, 'BIPOLAR': 1}} for ch in ALLOWED_OUTPUT_CHANNELS})
+COMMANDS372.update({f'device-settings:ls372:heater-channel-{ch}:setpoint': {'command': 'SETP', 'vals': [0, 4]} for ch in ALLOWED_OUTPUT_CHANNELS})
+COMMANDS372.update({f'device-settings:ls372:heater-channel-{ch}:gain': {'command': 'PID', 'vals': [0, 1000]} for ch in ALLOWED_OUTPUT_CHANNELS})
+COMMANDS372.update({f'device-settings:ls372:heater-channel-{ch}:integral': {'command': 'PID', 'vals': [0, 10000]} for ch in ALLOWED_OUTPUT_CHANNELS})
+COMMANDS372.update({f'device-settings:ls372:heater-channel-{ch}:derivative': {'command': 'PID', 'vals': [0, 2500]} for ch in ALLOWED_OUTPUT_CHANNELS})
+COMMANDS372.update({f'device-settings:ls372:heater-channel-{ch}:range': {'command': 'RANGE', 'vals': {'OFF': 0, 'ON': True,
+ 'RANGE_31_POINT_6_MICRO_AMPS': 1,
+ 'RANGE_100_MICRO_AMPS': 2,
+ 'RANGE_316_MICRO_AMPS': 3,
+ 'RANGE_1_MILLI_AMP': 4,
+ 'RANGE_3_POINT_16_MILLI_AMPS': 5,
+ 'RANGE_10_MILLI_AMPS': 6,
+ 'RANGE_31_POINT_6_MILLI_AMPS': 7,
+ 'RANGE_100_MILLI_AMPS': 8}} for ch in ALLOWED_OUTPUT_CHANNELS})
+
+
+class LakeShore336Command:
+    def __init__(self, schema_key, value=None):
+        """
+        Initializes a LakeShore336Command. Takes in a redis device-setting:* key and desired value an evaluates it for
+        its type, the mapping of the command, and appropriately sets the mapping|range for the command. If the setting
+        is not supported, raise a ValueError.
+        """
+
+        if schema_key not in COMMANDS372.keys():
+            raise ValueError(f'Unknown command: {schema_key}')
+
+        self.range = None
+        self.mapping = None
+        self.value = value
+        self.setting = schema_key
+
+        self.command = COMMANDS372[self.setting]['command']
+        setting_vals = COMMANDS372[self.setting]['vals']
+
+        if isinstance(setting_vals, dict):
+            self.mapping = setting_vals
+        else:
+            self.range = setting_vals
+        self._vet()
+
+    def _vet(self):
+        value = self.value
+
+        if self.mapping is not None:
+            if value not in self.mapping:
+                raise ValueError(f"Invalid value: {value} Options are: {list(self.mapping.keys())}.")
+        elif self.range is not None:
+            try:
+                self.value = float(value)
+            except ValueError:
+                raise ValueError(f'Invalid value {value}, must be castable to float.')
+            if not self.range[0] <= self.value <= self.range[1]:
+                raise ValueError(f'Invalid value {value}, must in {self.range}.')
+        else:
+            self.value = str(value)
+
+    def __str__(self):
+        return f"{self.setting_field}->{self.command_value}"
+
+    @property
+    def command_code(self):
+        return self.command
+
+    @property
+    def setting_field(self):
+        return self.setting.split(":")[-1].replace('-', '_')
+
+    @property
+    def command_value(self):
+        if self.mapping is not None:
+            return self.mapping[self.value]
+        else:
+            return self.value
+
+    @property
+    def desired_setting(self):
+        return {self.setting_field: self.command_value}
+
+    @property
+    def channel(self):
+        id_str = self.setting.split(":")[2]
+        return id_str[-1] if 'channel' in id_str else None
+
+    @property
+    def curve(self):
+        id_str = self.setting.split(":")[2]
+        return id_str[-1] if 'curve' in id_str else None
 
 
 class LakeShore372(LakeShoreMixin, Model372):
