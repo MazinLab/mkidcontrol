b8cbb6060c1e8eaf2a77cf18977acf87e7100bd6
Noah Swimmer <nswimmer@physics.ucsb.edu>
2022-11-01 21:49:49 -0700

    Update install.sh script to use the forked Mazinlab fliusb repos so we can control them and they do not go out of date

--- install.sh
+++ install.sh
@@ -120,36 +120,31 @@ sudo apt-get update
 sudo apt-get install indi-fli
 
 # FLI SDK install + USB driver setup + Python Distro
-
+# NOTE: This is for Linux-5.x.x kernels. If you move to 6 you may need to update the fliusb.c code for the driver
 # The following will get the libfli.so that's needed to run FLI code in linux
 wget https://www.flicamera.com/downloads/sdk/libfli-1.104.zip
 unzip libfli-1.104.zip
+git clone https://github.com/MazinLab/python-FLI.git
+cp python-FLI/libfli.so-1.104_Makefile/Makefile libfli-1.104/
 cd libfli-1.104
-git clone https://github.com/cversek/python-FLI.git
-cp python-FLI/libfli.so-1.104_Makefile/Makefile .
 make clean
 make
 sudo cp libfli.so /usr/local/lib
 
 # The following will install the python FLI library
-cd ~
-git clone https://github.com/SAIL-Labs/python-FLI.git
-cd python-FLI
+cd ~/python-FLI
 python setup.py install
 
-# The following will install the usb driver
+# The following will create and install the FLI usb driver
+# Create
+cd ~
 mkdir rts2fliusb
 cd rts2fliusb
-git clone https://github.com/RTS2/fliusb.git
+git clone https://github.com/MazinLab/fliusb.git
 cd fliusb/fliusb
-# THE STEPS THAT ARE COMMENTED OUT MUST BE DONE MANUALLY OR THE COMPILATION WILL FAIL
-# Modify the fliusb.c file
-# Add '#include <linux/mmap_lock.h>' below the line '#include <linux/slab.h>'
-# Change all instances of 'mmap_sem' to 'mmap_lock' (this driver was for linux-4.x.x kernels, we are using linux-5.15.0)
-# save the changes to the fliusb.c file
 make clean
 sudo make
-
+# Install
 sudo mkdir /lib/modules/5.15.0-41-generic/extra/
 sudo cp fliusb.ko /lib/modules/5.15.0-41-generic/extra/
 sudo insmod /usr/lib/modules/5.15.0-41-generic/extra/fliusb.ko
