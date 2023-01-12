6c0d11bc4bd0ed7bbb0da94cfc22135396b6974c
Noah Swimmer <nswimmer@physics.ucsb.edu>
2023-01-12 11:32:19 -0800

    Added ConexManager functionality

--- mkidcontrol/agents/xkid/conexAgent.py
+++ mkidcontrol/agents/xkid/conexAgent.py
@@ -60,8 +60,6 @@ class Conex(SerialDevice):
 
         self.ctrlN = controller  # Controller can be an int between 1-31 inclusive or a string of 1 to 2 characters that
         # represents the possible number values (i.e. 1, "1", and "01" will all work)
-        self.sn = None
-        self.firmware = None
 
         self.u_lower_limit = -np.inf
         self.v_lower_limit = -np.inf
@@ -348,7 +346,7 @@ class ConexManager:
         pos = (np.NaN, np.NaN)
         status = ''
         try:
-            status = self.conex.status
+            status = self.conex.status()
             pos = self.conex.position()
             log.debug(f"Conex: {status[1]} @ pos {pos}")
         except (IOError, serial.SerialException):
@@ -356,6 +354,353 @@ class ConexManager:
             self._halt_dither = True
             self._updateState('Offline')
         return {'state': self.state, 'pos': pos, 'conexstatus': status, 'limits': self.conex.limits}
+
+    def queryMove(self):
+        """
+        Checks to see if move completed
+
+        It should be thread safe. Even if you hit the move button several times really fast
+
+        OUTPUTS:
+            dictionary {'completed':True/False, 'status':self.cur_status}
+        """
+        if self._completedMoves > 0:  # don't lock if no moves completed. Reading is thread safe
+            with self._rlock:  # if at least one move completed then lock
+                if self._completedMoves > 0:  # need to check again for thread safety (maybe started two moves but only 1 completed)
+                    self._completedMoves -= 1
+                    self._startedMove -= 1
+                    self._startedMove = max(0, self._startedMove)
+                    return {'completed': True, 'status': self.cur_status}
+        return {'completed': False, 'status': self.cur_status}
+
+    def queryDither(self):
+        """
+        returns the dictionary containing information about an ongoing or completed dither
+
+        keys:
+            status - The current status of the conex manager
+            estTime - ?? Not implemented right now
+            dither - A dictionary containing the oldest completed dither that hasn't been popped yet
+                     see dither() output
+                     If no completed dithers then None
+            completed - True or False
+        """
+        dith = None
+        estTime = 0
+        completed = False
+        if len(self._completed_dithers) > 0:  # Reading is thread safe
+            with self._rlock:  # only lock if at least one dither completed
+                try:
+                    dith = self._completed_dithers.pop(0)
+                    completed = True
+                except IndexError:
+                    pass
+        if dith is None:  # check if a dither was popped
+            estTime = time.time() + 1  # estimated unix time of when dither will complete
+        return {'status': self.cur_status, 'estTime': estTime, 'dither': dith, 'completed': completed}
+
+    def stop(self, wait=False):
+        """
+        stops the current movement or dither
+
+        if wait is False then it forcibly writes to the conex to tell it to stop motion
+
+        after that it waits for the movement thread to finish
+        """
+        log.debug('stopping conex')
+
+        if self._movement_thread is not None and self._movement_thread.is_alive():
+            with self._rlock:
+                self._halt_dither = True
+                if not wait:
+                    self.conex.stop()  # puts conex in ready state so that _movement thread will finish
+                self._updateState('Stopped')
+                self.cur_status = self.status()
+            self._movement_thread.join()  # not in rlock
+            with self._rlock:
+                self.cur_status = self.status()  # could change in other thread
+        else:
+            with self._rlock:
+                self.cur_status = self.status()
+        return self.cur_status
+
+    def start_dither(self, dither_dict):
+        """
+        Starts dither in a new thread
+        """
+        log.debug('starting dither')
+        self.stop(wait=False)  # stop whatever we were doing before (including a previous dither)
+        with self._rlock:
+            self.cur_status = self.status()
+            if self.cur_status['state'] == 'Offline': return False
+            self._halt_dither = False
+            self._startedMove = 0
+        self._preDitherPos = self.cur_status['pos']
+        self._movement_thread = threading.Thread(target=self.dither_two_point, args=(dither_dict,), name="Dithering thread")
+        self._movement_thread.daemon = True
+        self._movement_thread.start()
+        return True
+
+    def dither_two_point(self, dither_dict):
+        """
+        INPUTS:
+            dither_dict - dictionary with keys:
+                        startx: (float) start x loc in conex degrees
+                        endx: (float) end x loc
+                        starty: (float) start y loc
+                        endy: (float) end y loc
+                        n: (int) number of steps in square grid
+                        t: (float) dwell time in seconds
+                        subStep: (float) degrees to offset for subgrid pattern
+                        subT: (float) dwell time for subgrid
+
+                        subStep and subT are optional
+
+        appends a dictionary to the self._completed_dithers attribute
+            keys - same as dither_dict but additionally
+                   it has keys (xlocs, ylocs, startTimes, endTimes)
+
+        """
+        points = dither_two_point_positions(dither_dict['startx'], dither_dict['starty'], dither_dict['stopx'],
+                                            dither_dict['stopy'], dither_dict['n'])
+
+        subDither = 'subStep' in dither_dict.keys() and dither_dict['subStep'] > 0 and \
+                    'subT' in dither_dict.keys() and dither_dict['subT'] > 0
+
+        x_locs = []
+        y_locs = []
+        startTimes = []
+        endTimes = []
+        for p in points:
+            startTime, endTime = self._dither_move(p[0], p[1], dither_dict['t'])
+            if startTime is not None:
+                x_locs.append(self.cur_status['pos'][0])
+                y_locs.append(self.cur_status['pos'][1])
+                startTimes.append(startTime)
+                endTimes.append(endTime)
+            if self._halt_dither: break
+
+            # do sub dither if neccessary
+            if subDither:
+                x_sub = [-dither_dict['subStep'], 0, dither_dict['subStep'], 0]
+                y_sub = [0, dither_dict['subStep'], 0, -dither_dict['subStep']]
+                for i in range(len(x_sub)):
+                    if self.conex.in_bounds((p[0] + x_sub[i], p[1] + y_sub[i])):
+                        startTime, endTime = self._dither_move(p[0] + x_sub[i], p[1] + y_sub[i], dither_dict['subT'])
+                        if startTime is not None:
+                            x_locs.append(self.cur_status['pos'][0])
+                            y_locs.append(self.cur_status['pos'][1])
+                            startTimes.append(startTime)
+                            endTimes.append(endTime)
+                    if self._halt_dither: break
+            if self._halt_dither: break
+
+        # Dither has completed (or was stopped prematurely)
+        if not self._halt_dither:  # no errors and not stopped
+            self.move(*self._preDitherPos)
+            with self._rlock:
+                if not self._halt_dither:  # still no errors nor stopped
+                    self._updateState("Idle")
+                self.cur_status = self.status()
+
+        dith = dither_dict.copy()
+        dith['xlocs'] = x_locs  # could be empty if errored out or stopped too soon
+        dith['ylocs'] = y_locs
+        dith['startTimes'] = startTimes
+        dith['endTimes'] = endTimes
+        with self._rlock:
+            self._completed_dithers.append(dith)
+
+    def dither(self, dither_dict):
+        """
+        INPUTS:
+            dither_dict - dictionary with keys:
+                        startx: (float) start x loc in conex degrees
+                        endx: (float) end x loc
+                        starty: (float) start y loc
+                        endy: (float) end y loc
+                        n: (int) number of steps in square grid
+                        t: (float) dwell time in seconds
+                        subStep: (float) degrees to offset for subgrid pattern
+                        subT: (float) dwell time for subgrid
+
+                        subStep and subT are optional
+
+        appends a dictionary to the self._completed_dithers attribute
+            keys - same as dither_dict but additionally
+                   it has keys (xlocs, ylocs, startTimes, endTimes)
+
+        """
+        # Ennabling single direction sweeps
+        if dither_dict['startx'] == dither_dict['endx']:
+            x_list = np.linspace(dither_dict['startx'], dither_dict['endx'], 1)
+        else:
+            x_list = np.linspace(dither_dict['startx'], dither_dict['endx'], dither_dict['n'])
+        if dither_dict['starty'] == dither_dict['endy']:
+            y_list = np.linspace(dither_dict['starty'], dither_dict['endy'], 1)
+        else:
+            y_list = np.linspace(dither_dict['starty'], dither_dict['endy'], dither_dict['n'])
+
+        subDither = 'subStep' in dither_dict.keys() and dither_dict['subStep'] > 0 and \
+                    'subT' in dither_dict.keys() and dither_dict['subT'] > 0
+
+        x_locs = []
+        y_locs = []
+        startTimes = []
+        endTimes = []
+        for x in x_list:
+            for y in y_list:
+                startTime, endTime = self._dither_move(x, y, dither_dict['t'])
+                if startTime is not None:
+                    x_locs.append(self.cur_status['pos'][0])
+                    y_locs.append(self.cur_status['pos'][1])
+                    startTimes.append(startTime)
+                    endTimes.append(endTime)
+                if self._halt_dither: break
+
+                # do sub dither if neccessary
+                if subDither:
+                    x_sub = [-dither_dict['subStep'], 0, dither_dict['subStep'], 0]
+                    y_sub = [0, dither_dict['subStep'], 0, -dither_dict['subStep']]
+                    for i in range(len(x_sub)):
+                        if self.conex.in_bounds((x + x_sub[i], y + y_sub[i])):
+                            startTime, endTime = self._dither_move(x + x_sub[i], y + y_sub[i], dither_dict['subT'])
+                            if startTime is not None:
+                                x_locs.append(self.cur_status['pos'][0])
+                                y_locs.append(self.cur_status['pos'][1])
+                                startTimes.append(startTime)
+                                endTimes.append(endTime)
+                        if self._halt_dither: break
+                if self._halt_dither: break
+            if self._halt_dither: break
+
+        # Dither has completed (or was stopped prematurely)
+        if not self._halt_dither:  # no errors and not stopped
+            self.move(*self._preDitherPos)
+            with self._rlock:
+                if not self._halt_dither:  # still no errors nor stopped
+                    self._updateState("Idle")
+                self.cur_status = self.status()
+
+        dith = dither_dict.copy()
+        dith['xlocs'] = x_locs  # could be empty if errored out or stopped too soon
+        dith['ylocs'] = y_locs
+        dith['startTimes'] = startTimes
+        dith['endTimes'] = endTimes
+        with self._rlock:
+            self._completed_dithers.append(dith)
+
+    def _dither_move(self, x, y, t):
+        """
+            Helper function for dither()
+
+            The state after this function call will be one of:
+                "error: ..." - If there there was an error during the move
+                "processing" - If everything worked
+        """
+        polltime = 0.1  # wait for dwell time but have to check if stop was pressed periodically
+        self.move(x, y)
+        if self._halt_dither: return None, None  # Stopped or error during move
+        self._updateState("Dither dwell for {:.1f} seconds".format(t))
+        # dwell at position
+        startTime = time.time()
+        dwell_until = startTime + t
+        endTime = time.time()
+        with self._rlock:
+            self.cur_status = self.status()
+        while self._halt_dither == False and endTime < dwell_until:
+            sleep = min(polltime, dwell_until - endTime)
+            time.sleep(max(sleep, 0))
+            endTime = time.time()
+        return startTime, endTime
+
+    def start_move(self, x, y):
+        """
+        Starts move in new thread
+        """
+        self.stop(
+            wait=False)  # If the user wants to move, then forcibly stop whatever we were doing before (indcluding dithers)
+        with self._rlock:
+            self.cur_status = self.status()
+            if self.cur_status['state'] == 'Offline': return False
+            self._startedMove += 1
+        # log.error("Starting move to {:.2f}, {:.2f}".format(x,y))
+        self._movement_thread = threading.Thread(target=self.move, args=(x, y,),
+                                       name='Move to ({}, {})'.format(x, y))
+        self._movement_thread.daemon = True
+        self._movement_thread.start()
+
+        return True
+
+    def move(self, x, y):
+        """
+        Tells conex to move and collects errors
+        """
+        self._updateState('Moving to {:.2f}, {:.2f}'.format(x, y))
+        try:
+            self.conex.move((x, y), blocking=True)  # block until conex is done moving (or stopped)
+            if self._startedMove > 0: self._updateState('Idle')
+            log.debug('moved to ({}, {})'.format(x, y))
+        except (IOError, serial.SerialException) as e:  # on timeout it raise IOError
+            self._updateState('Error: move to {:.2f}, {:.2f} failed'.format(x, y))
+            self._halt_dither = True
+            log.error('Error on move', exc_info=True)
+        except:  # I dont think this should happen??
+            self._updateState('Error: move to {:.2f}, {:.2f} failed'.format(x, y))
+            self._halt_dither = True
+            log.error('Unexpected error on move', exc_info=True)
+        if self._startedMove > 0:
+            with self._rlock:
+                self.cur_status = self.status()
+                self._completedMoves += 1
+
+
+def dither_two_point_positions(start_x, start_y, stop_x, stop_y, user_n_steps, single_pixel_move=0.015):
+    if user_n_steps == 1:
+        log.error('Number of steps must be greater than one!')
+        return
+    if stop_y == start_y and stop_x == start_x:
+        log.error('No movement specified in x or y')
+        return
+
+    n_steps = user_n_steps - 1
+    points = []
+    interval_x = ((stop_x - (0.5 * single_pixel_move)) - start_x) / (n_steps / 2)
+    interval_y = ((stop_y - (0.5 * single_pixel_move)) - start_y) / (n_steps / 2)
+
+    x_list = np.arange(start_x, stop_x, interval_x)
+    y_list = np.arange(start_y, stop_y, interval_y)
+    if start_x == stop_x:
+        x_list = np.zeros(n_steps)
+    if start_y == stop_y:
+        y_list = np.zeros(n_steps)
+
+    offset_x = np.round(x_list + (interval_x / 2 + 0.5 * single_pixel_move), 3)
+    offset_y = np.round(y_list + (interval_y / 2 + 0.5 * single_pixel_move), 3)
+    x_grid = np.round(np.sort(np.concatenate((x_list, offset_x[offset_x <= round(stop_x, 4)]))), 3)
+    y_grid = np.round(np.sort(np.concatenate((y_list, offset_y[offset_y <= round(stop_y, 4)]))), 3)
+    cycle = 1
+
+    if not (np.all(x_grid == 0) or np.all(y_grid == 0)):
+        points.append((start_x, start_y))
+
+    while cycle <= n_steps:
+        for i in range(cycle + 1):
+            rev = cycle - i
+            points.append((x_grid[i], y_grid[rev]))
+        cycle += 1
+    cycle = 1
+    second_points = []
+    while cycle <= (n_steps - 1):
+        for i in range(cycle + 1):
+            rev = cycle - i
+            second_points.insert(0, (x_grid[-1 - i], y_grid[-1 - rev]))
+        cycle += 1
+    points = points + second_points
+    if not (np.all(x_grid == 0) or np.all(y_grid == 0)):
+        points.append((x_grid[-1], y_grid[-1]))
+    return points
+
 
 if __name__ == "__main__":
 
