109861aa4abf37264a8319e8c463f571993f02f1
Noah Swimmer <nswimmer@physics.ucsb.edu>
2023-02-07 14:23:11 -0800

    packetmaster3Test.py almost completely working

--- mkidcontrol/tests/packetmaster3Test.py
+++ mkidcontrol/tests/packetmaster3Test.py
@@ -1,18 +1,24 @@
 from mkidcontrol.packetmaster3.packetmaster import Packetmaster
+import time
+from mkidcore.objects import Beammap
+import numpy as np
 
-roachNums = [115,....]
+SHAREDIMAGE_LATENCY = 0.55 #0.53 #latency fudge factor for sharedmem, taken from dashboard.py
+
+roachNums = [115, 116, 117, 118, 119, 120, 121, 122]
 captureport = 50000
 offline = False
 imgcfg = {'dashboard': dict(use_wave=False, wave_start=700, wave_stop=1500, n_wave_bins=1)}
-beammap = mkidcore.beammap.Beammap()  #beammap object
-wavecal_file = '/home/baileyji/mec/2019-01-13 10529f0c026d91f8361e8d1bbe93699fcccb.npz'
+beammap = Beammap(specifier="XKID")  #beammap object
+wavecal_file = '/home/kids/mkidcontrol/mkidcontrol/tests/wavecals/wavecal0_4b22ee4c_f4d1f81f.wavecal.npz'
 bindir = '/some/path'
 
 packetmaster = Packetmaster(len(roachNums), captureport, useWriter=not offline, sharedImageCfg=imgcfg,
                                  beammap=beammap, forwarding=None, recreate_images=True)
 
-wvl_coeffs = np.load(wavecal_file)
-packetmaster.applyWvlSol(wvlCoeffs, beammap)
+# TODO: These lines break because you need an older-style wavecal. Need to look into this
+# wvl_coeffs = np.load(wavecal_file)
+# packetmaster.applyWvlSol(wvl_coeffs, beammap)
 
 liveimage = packetmaster.sharedImages['dashboard']
 
@@ -20,7 +26,6 @@ liveimage.startIntegration(startTime=time.time() - SHAREDIMAGE_LATENCY, integrat
 data = liveimage.receiveImage()
 
 liveimage.set_useWvl(True)
-
 
 packetmaster.startWriting(binDir=bindir)
 packetmaster.stopWriting()
