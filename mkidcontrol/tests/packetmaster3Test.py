0d8ea4ebf44212f131d16316a1d0661769934550
Noah Swimmer <nswimmer@physics.ucsb.edu>
2023-02-07 16:32:20 -0800

    fixed issue where packmaster will segfault if the bin file directory does not exist

--- mkidcontrol/tests/packetmaster3Test.py
+++ mkidcontrol/tests/packetmaster3Test.py
@@ -19,7 +19,10 @@ offline = False
 imgcfg = {'dashboard': dict(use_wave=False, wave_start=700, wave_stop=1500, n_wave_bins=1)}
 beammap = Beammap(specifier="XKID")  #beammap object
 wavecal_file = '/home/kids/mkidcontrol/mkidcontrol/tests/wavecals/wavecal0_4b22ee4c_f4d1f81f.wavecal.npz'
-bindir = '/some/path'
+bindir = '/data/XKID/testdata'
+
+if not os.path.exists(bindir):
+    os.mkdir(bindir)
 
 packetmaster = Packetmaster(len(roachNums), captureport, useWriter=not offline, sharedImageCfg=imgcfg,
                                  beammap=beammap, forwarding=None, recreate_images=True)
