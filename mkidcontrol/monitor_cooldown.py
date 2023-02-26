be443312fbd449e5ad0182fa13a89a58059858fa
nswimmer <nswimmer@physics.ucsb.edu>
2023-02-26 12:02:45 -0300

    added convenience function for plotting sensor values

--- mkidcontrol/monitor_cooldown.py
+++ mkidcontrol/monitor_cooldown.py
@@ -52,6 +52,37 @@ def plot_cooldown_temperatures(start=None, end=None):
     plt.ylim(0, 300)
 
 
+def plot_cooldown_resistances(start=None, end=None):
+    """
+    Create a figure with thermometry data from the 4 temperature stage sensors (50k, 3k, 1k, and Device).
+    Takes in start and end timestamps which must be integer UNIX timestamps in milliseconds.
+    If start is none, default to 24 hours previous to the query
+    If end is none, default to most recent data recorded
+
+    Does not show, plt.show() must be called after this function to see the figure
+    """
+    redis.setup_redis(ts_keys=TS_KEYS)
+
+    if end is None:
+        end = '+'
+
+    if start is None:
+        start = int((time.time() - 24 * 60 * 60) * 1000)
+
+    # Query each temperature stage which will be a 2 column array of form [[time1, temp1], [time2, temp2], ...]
+    o = np.array(redis.redis_ts.range('status:temps:1k-stage:resistance', start, end))
+    d = np.array(redis.redis_ts.range('status:temps:device-stage:resistance', start, end))
+
+    # Create a figure with data from each of the 4 sensors
+    plt.figure()
+    plt.plot(o[:, 0] / 1000 / 3600 - o[:, 0][0] / 1000 / 3600, o[:, 1], label='1K')
+    plt.plot(d[:, 0] / 1000 / 3600 - d[:, 0][0] / 1000 / 3600, d[:, 1], label='Device')
+    plt.legend()
+    plt.ylabel('Resistance (Ohms)')
+    plt.xlabel('Time (Hours)')
+    # plt.ylim()
+
+
 if __name__ == "__main__":
 
     header = ['Time', 'Timestamp', '50k T', '50k V', '3k T', '3k V', '1k T', '1k R', 'Device T', 'Device R', 'Magnet Current', 'Magnet Field']
