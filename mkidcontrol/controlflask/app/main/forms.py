4526620f7c3d93df20b17c8fabd29132d92bfcd8
nswimmer <nswimmer@physics.ucsb.edu>
2023-03-02 23:20:26 -0300

    We must pick a better default for the observation name

--- mkidcontrol/controlflask/app/main/forms.py
+++ mkidcontrol/controlflask/app/main/forms.py
@@ -59,7 +59,7 @@ class FocusForm(FlaskForm):
 
 class ObsControlForm(FlaskForm):
     obsStartStop = SubmitField("")
-    obsName = StringField("Name", default='---')
+    obsName = StringField("Name", default='---')  # TODO: Pick a better default
     wavecal = SubmitField("Wavecal")
     obs_duration = FloatField("Duration (s):", default=60)
     flat = SubmitField("Flat")
